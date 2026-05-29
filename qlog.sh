5a4c88f3c22c9962d5ee6136106f65403120c0b0
BotPhil01 <110259538+BotPhil01@users.noreply.github.com>
2026-05-29 13:23:41 +0100

    Add: verbose SCAT decoding with qdb hash

--- qlog.sh
+++ qlog.sh
@@ -4,11 +4,9 @@ QD="$QUECTEL_DIR"
  
 function usage {
     echo "usage: script.sh outfile"
-    echo "       logs will be stored in quectel_qlog/out/outfile as outfile.qmdl2"
+    echo "       logs will be stored in quectel_qlog/out/outfile"
     exit 1
 }
-
-echo $#
 
 if [[ $# < 1 || "$1" == "--help" || "$1" == "-h" ]]; then
     usage
@@ -22,4 +20,10 @@ logs="$QD/logs/$1"
 
 $QD/QLog -p /dev/ttyUSB0 -s $QD -f $QD/../conf/defaultNR5G1216.cfg -q 
 
-mv $QD/*.qmdl2 $logs/$1.qmdl2
+QDB="$logs/$1.qdb"
+QMDL2="$logs/$1.qmdl2"
+
+mv $QD/*.qmdl2 $QMDL2
+mv $QD/*.qdb $QDB
+
+scat -t qc -d $QDML2 --qsr4-hash $QDB -F $logs/$1.pcap
